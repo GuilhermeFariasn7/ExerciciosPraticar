2294032421c714303c46ea7b05cf943f39231d0a
Guilherme Farias Nazário <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-28 08:53:57 -0300

    Create Exer 2.py

---
+++ Exer 2.py
@@ -0,0 +1,9 @@
+N1 = float(input("Digite um número real: "))
+
+print("Seu número foi: {:.2f}".format(N1)) 
+
+
+"""n1 = float(input("digite um valor: "))
+n2 = float(input("Digite um valor: "))
+print("a divisão do numero {} e do numero {} resulta no numero de {:.3f}".format(n1,n2,(n1/n2))) 
+print("a divisão do numero {} e do numero {} resulta no numero de {:.2f}".format(n1,n2,(n1/n2))) """
