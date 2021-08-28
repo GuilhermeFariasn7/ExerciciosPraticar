3a5de4d13a0706bd91f70c4b4be5afe83a7f02d8
Guilherme Farias Nazário <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-28 10:07:03 -0300

    Correção

--- Exer 14.py
+++ Exer 14.py
@@ -1,3 +1,4 @@
 G = float(input("Digite um angulo em graus: "))
 R = ((G * 3.14)/180)
 print("O angulo {} em radianos ficarah: {:.4f}".format(G,R))
+#as que são parecidas eu n fiz por ser quase a mesma coisa só mudando a ordem.
