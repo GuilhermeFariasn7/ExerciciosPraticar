04059b951782c0a717937af244fab3e95a912e29
Guilherme Farias Nazário <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-28 09:03:52 -0300

    Create Exer 3.py

---
+++ Exer 3.py
@@ -0,0 +1,8 @@
+print(f'\033[0;33m *Digite 3 valores inteiros*')
+N1 = int(input("Digite o primeiro valor: "))
+N2 = int(input("Digte o segundo valor: "))
+N3 = int(input("Digite o terceiro valor: "))
+
+print("A soma dos valores {}, {} e {} são {}".format(N1,N2,N3,(N1+N2+N3)))
+
+#print(f'\033[0;31m aaa')
