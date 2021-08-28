d3de297d05fa2209dc330255280f3c32852b64ee
Guilherme Farias Nazário <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-28 09:55:09 -0300

    Create Exer 7.py

---
+++ Exer 7.py
@@ -0,0 +1,4 @@
+F = float(input("Digite um valor em fahrenheit a ser convertido para celsius: "))
+
+C = (5*(F-32)/9)
+print("A temperatura convertida de fahrenheit({:.2f}) para celsius é: {}".format(F,C)) 
